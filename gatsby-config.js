9eb2cbcbc43074dbf8bea0f76c275f216c91b83d
Braden Watkins <bradenw@qualtrics.com>
2020-01-11 16:18:43 -0700

    Add RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,58 @@ module.exports = {
         logo: "./src/static/favicon.png",
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `          
+          {
+            site {
+              siteMetadata {
+                siteTitle
+                siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdxPost } }) => {
+              return allMdxPost.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdxPost(sort: {order: DESC, fields: date}) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      slug
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "bradenwatkins.dev RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
   ],
 }
