ce82b97aba105e1b027b81e0519a96353995725b
Braden Watkins <bradenw@qualtrics.com>
2020-01-18 14:56:55 -0700

    Add essentials

--- content/projects.js
+++ content/projects.js
@@ -4,6 +4,13 @@ const projects = [
     to: "/raffle",
     description: "A random name picker made with pure HTML/CSS/JS",
   },
+  {
+    name: "Essentials Extension Pack: VsCode",
+    href:
+      "https://marketplace.visualstudio.com/items?itemName=bradenwatkins.essentials",
+    description:
+      "Essential extensions to make working with VsCode easier and more delightful",
+  },
 ]
 
 export default projects
